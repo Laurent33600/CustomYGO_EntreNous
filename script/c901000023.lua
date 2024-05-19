b74118ea9fe8fedeff1458e3e958bb869b71eb9d
Laurent <laurent.lopez33600@gmail.com>
2024-05-19 23:54:03 +0200

    x

--- script/c901000023.lua
+++ script/c901000023.lua
@@ -2,67 +2,72 @@
 local s,id=GetID()
 
 function s.chainfilter(re,tp,cid)
-	return not (re:IsActiveType(TYPE_MONSTER) and Duel.IsMainPhase())
+    return not (re:IsActiveType(TYPE_MONSTER) and Duel.IsMainPhase())
 end
 
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase() == PHASE_SUMMON and e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+    return Duel.GetCurrentPhase() == PHASE_SUMMON and e:GetHandler():IsPreviousLocation(LOCATION_HAND)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	local options = {
-		[1] = {desc = aux.Stringid(id,1), func = s.addToHand},
-		[2] = {desc = aux.Stringid(id,2), func = s.banish},
-		[3] = {desc = aux.Stringid(id,3), func = nil} -- Cancel option
-	}
+    if chk==0 then return true end
+    Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+    local options = {
+        [1] = {desc = aux.Stringid(id,1), code = 1}, -- Add to hand
+        [2] = {desc = aux.Stringid(id,2), code = 2}  -- Banish
+    }
 
-	local optionText = ""
-	for i, option in ipairs(options) do
-		optionText = optionText .. "[" .. i .. "] " .. option.desc .. "\n"
-	end
+    local optionText = {}
+    for i, option in ipairs(options) do
+        table.insert(optionText, option.desc)
+    end
 
-	local selectedOption = Duel.SelectOption(tp,table.unpack(optionText))
-
-	if selectedOption == 3 then return end -- Cancel option
-
-	options[selectedOption].func(e,tp)
+    local selectedOption = Duel.SelectOption(tp,table.unpack(optionText))
+    if selectedOption == 0 then
+        local op = options[1].code
+        e:SetCategory(CATEGORY_TOHAND)
+        e:SetOperation(s.addToHand)
+    else
+        local op = options[2].code
+        e:SetCategory(CATEGORY_REMOVE)
+        e:SetOperation(s.banish)
+    end
 end
 
-function s.addToHand(e,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		if tc:IsAbleToHand() then
-			Duel.SendtoHand(tc,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
-		end
-	end
+function s.addToHand(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+    local tc=g:GetFirst()
+    if tc then
+        if tc:IsAbleToHand() then
+            Duel.SendtoHand(tc,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,tc)
+        end
+    end
 end
 
-function s.banish(e,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		if tc:IsAbleToRemove() then
-			Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
-		end
-	end
+function s.banish(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+    local tc=g:GetFirst()
+    if tc then
+        if tc:IsAbleToRemove() then
+            Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+        end
+    end
 end
 
 function s.filter(c)
-	return c:IsSetCard(3856) and c:IsType(TYPE_TRAP)
+    return c:IsSetCard(3856) and c:IsType(TYPE_TRAP)
 end
 
 function s.initial_effect(c)
-	-- Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	c:RegisterEffect(e1)
-	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,s.chainfilter)
+    -- Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetCondition(s.condition)
+    e1:SetTarget(s.target)
+    c:RegisterEffect(e1)
+    Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,s.chainfilter)
 end
