2b69e3777d01efde5b29016110d3d3ce3f50c807
Laurent <laurent.lopez33600@gmail.com>
2024-05-19 23:48:19 +0200

    x

--- script/c901000023.lua
+++ script/c901000023.lua
@@ -59,9 +59,8 @@ end
 function s.initial_effect(c)
 	-- Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
