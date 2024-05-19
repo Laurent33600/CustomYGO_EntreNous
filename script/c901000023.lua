925e6cbb9c57dd3fccc7cc88e9d3e4bf3f6d10a1
Laurent <laurent.lopez33600@gmail.com>
2024-05-20 00:08:13 +0200

    x

--- script/c901000023.lua
+++ script/c901000023.lua
@@ -17,8 +17,39 @@ end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return true end
-    e:SetCategory(CATEGORY_REMOVE)
-    e:SetOperation(s.banish)
+    Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+    local options = {
+        [1] = {desc = aux.Stringid(id,1), code = 1}, -- Add to hand
+        [2] = {desc = aux.Stringid(id,2), code = 2}  -- Banish
+    }
+
+    local optionText = {}
+    for i, option in ipairs(options) do
+        table.insert(optionText, option.desc)
+    end
+
+    local selectedOption = Duel.SelectOption(tp,table.unpack(optionText)) + 1
+
+    local op = options[selectedOption].code
+    if op == 1 then
+        e:SetCategory(CATEGORY_TOHAND)
+        e:SetOperation(s.addToHand)
+    elseif op == 2 then
+        e:SetCategory(CATEGORY_REMOVE)
+        e:SetOperation(s.banish)
+    end
+end
+
+function s.addToHand(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+    local tc=g:GetFirst()
+    if tc then
+        if tc:IsAbleToHand() then
+            Duel.SendtoHand(tc,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,tc)
+        end
+    end
 end
 
 function s.banish(e,tp,eg,ep,ev,re,r,rp)
@@ -33,5 +64,5 @@ function s.banish(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.filter(c)
-    return c:IsSetCard(3856) and c:IsType(TYPE_TRAP) and c:IsAbleToRemove()
+    return c:IsSetCard(3856) and c:IsType(TYPE_TRAP) and c:IsAbleToHand()
 end
