58243e5dc7dd1403138f91824e6fc6fe0aab15a6
Laurent <laurent.lopez33600@gmail.com>
2024-05-20 17:23:10 +0200

    x

---
+++ script/c901001005.lua
@@ -0,0 +1,65 @@
+-- Catalyseur de la Fiole Disparue
+local s, id = GetID()
+
+function s.initial_effect(c)
+    -- Special Summon itself
+    local e1 = Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id, 0))
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1, id)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+
+    -- Activate Trap from Deck
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 1))
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetCountLimit(1, id + 1)
+    e2:SetTarget(s.acttg)
+    e2:SetOperation(s.actop)
+    c:RegisterEffect(e2)
+    local e3 = e2:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e3)
+end
+
+function s.spcon(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType, TYPE_TRAP), tp, LOCATION_GRAVE, 0, 1, nil)
+end
+
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and e:GetHandler():IsCanBeSpecialSummoned(e, 0, tp, false, false) end
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
+end
+
+function s.spop(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
+    end
+end
+
+function s.actfilter(c)
+    return c:IsSetCard(3856) and c:IsType(TYPE_TRAP) and c:GetActivateEffect():IsActivatable(tp, true, true)
+end
+
+function s.acttg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.actfilter, tp, LOCATION_DECK, 0, 1, nil) end
+end
+
+function s.actop(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.Banish(c, POS_FACEUP, REASON_EFFECT)
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TOACTIVATE)
+        local g = Duel.SelectMatchingCard(tp, s.actfilter, tp, LOCATION_DECK, 0, 1, 1, nil)
+        if #g > 0 then
+            Duel.Activate(g:GetFirst())
+        end
+    end
+end
