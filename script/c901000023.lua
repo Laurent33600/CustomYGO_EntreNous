17f33523de5df9a3cfc798209a88aed4d9c86831
Laurent <laurent.lopez33600@gmail.com>
2024-05-20 08:41:32 +0200

    x

--- script/c901000023.lua
+++ script/c901000023.lua
@@ -4,49 +4,51 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-    -- Add "Fiole Disparue" archetype setcode
+    -- When this card is Normal Summoned: add 1 "Fiole Disparue" Trap Card from your Deck to your hand, or banish it.
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_ADD_SETCODE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetValue(0x3856)
+    e1:SetDescription(aux.Stringid(id, 0))
+    e1:SetCategory(CATEGORY_TOHAND + CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1, id)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
-
-    -- Effect 1: When this card is Normal Summoned: add 1 "Fiole Disparue" Trap Card from your Deck to your hand, or banish it.
-    local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_REMOVE)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetCode(EVENT_SUMMON_SUCCESS)
-    e2:SetCountLimit(1,id)
-    e2:SetTarget(s.thtg)
-    e2:SetOperation(s.thop)
-    c:RegisterEffect(e2)
 end
 
--- Effect 1: Target and operation functions
-function s.thfilter(c)
-    return c:IsSetCard(0x3856) and c:IsType(TYPE_TRAP) and c:IsAbleToHand()
+function s.filter(c)
+    return c:IsSetCard(0x3856) and c:IsType(TYPE_TRAP) and (c:IsAbleToHand() or c:IsAbleToRemove())
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+
+function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_DECK, 0, 1, nil) end
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
+    Duel.SetOperationInfo(0, CATEGORY_REMOVE, nil, 1, tp, LOCATION_DECK)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPTION)
-    local option = Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-    if option == 0 then
-        local g = Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-        if #g > 0 then
-            Duel.SendtoHand(g,nil,REASON_EFFECT)
-            Duel.ConfirmCards(1-tp,g)
+
+function s.operation(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+    local g = Duel.SelectMatchingCard(tp, s.filter, tp, LOCATION_DECK, 0, 1, 1, nil)
+    if #g > 0 then
+        local tc = g:GetFirst()
+        local b1 = tc:IsAbleToHand()
+        local b2 = tc:IsAbleToRemove()
+        local op = 0
+        if b1 and b2 then
+            op = Duel.SelectOption(tp, aux.Stringid(id, 1), aux.Stringid(id, 2))
+        elseif b1 then
+            op = Duel.SelectOption(tp, aux.Stringid(id, 1))
+        elseif b2 then
+            op = Duel.SelectOption(tp, aux.Stringid(id, 2)) + 1
+        else
+            return
         end
-    else
-        local g = Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-        if #g > 0 then
-            Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+        if op == 0 then
+            Duel.SendtoHand(tc, nil, REASON_EFFECT)
+            Duel.ConfirmCards(1 - tp, tc)
+        else
+            Duel.Remove(tc, POS_FACEUP, REASON_EFFECT)
         end
     end
 end
