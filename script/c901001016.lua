546fc7d354569a87160539e905d1805b7695eb7c
Laurent <laurent.lopez33600@gmail.com>
2024-06-09 02:30:54 +0200

    x

--- script/c901001016.lua
+++ script/c901001016.lua
@@ -2,28 +2,44 @@
 -- Scripted by [Your Name or Username]
 
 local s,id=GetID()
+
 function s.initial_effect(c)
-    -- Special Summon condition
+    -- Count Trap card activations
+    if not s.global_check then
+        s.global_check=true
+        s[0]=0
+        s[1]=0
+        local ge1=Effect.CreateEffect(c)
+        ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        ge1:SetCode(EVENT_CHAINING)
+        ge1:SetOperation(s.checkop)
+        Duel.RegisterEffect(ge1,0)
+        local ge2=Effect.CreateEffect(c)
+        ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        ge2:SetCode(EVENT_CHAIN_SOLVED)
+        ge2:SetOperation(s.resetop)
+        Duel.RegisterEffect(ge2,0)
+    end
+
+    -- Special Summon (Quick Effect)
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
     e1:SetCondition(s.spcon)
-    e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
     
-    -- Negate effects
+    -- Negate Effect
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_NEGATE)
+    e2:SetCategory(CATEGORY_DISABLE)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_SUMMON_SUCCESS)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-    e2:SetCountLimit(1,id+1)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetCountLimit(1,{id,1})
     e2:SetTarget(s.negtg)
     e2:SetOperation(s.negop)
     c:RegisterEffect(e2)
@@ -32,43 +48,51 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,1,nil,TYPE_TRAP)
-end
-
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    if e:GetHandler():IsRelateToEffect(e) then
-        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+    if re:IsActiveType(TYPE_TRAP) and re:IsHasType(EFFECT_TYPE_ACTIVATE) then
+        s[ep]=s[ep]+1
     end
 end
 
+function s.resetop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetCurrentPhase()==PHASE_END then
+        s[0]=0
+        s[1]=0
+    end
+end
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return s[tp]>0
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+end
+
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) end
-    if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-    local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_MZONE,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_NEGATE,g,1,0,0)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+    if chk==0 then return Duel.IsExistingTarget(aux.disfilter1,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,aux.disfilter1,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
 
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+    if tc and tc:IsRelateToEffect(e) and not tc:IsDisabled() then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
         local e1=Effect.CreateEffect(e:GetHandler())
         e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
         e1:SetCode(EFFECT_DISABLE)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
         tc:RegisterEffect(e1)
         local e2=Effect.CreateEffect(e:GetHandler())
         e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
         e2:SetCode(EFFECT_DISABLE_EFFECT)
         e2:SetValue(RESET_TURN_SET)
-        e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
         tc:RegisterEffect(e2)
     end
 end
