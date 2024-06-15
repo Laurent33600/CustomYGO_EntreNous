2e862700099ded59e905ab9f87c8dd3910b7f218
Laurent <laurent.lopez33600@gmail.com>
2024-06-15 11:16:06 +0200

    x

--- script/c901001022.lua
+++ script/c901001022.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_SPSUMMON_PROC)
     e1:SetRange(LOCATION_EXTRA)
-    e1:SetTarget(s.target)
+    e1:SetCondition(s.sprcon)
     e1:SetOperation(s.sprop)
     c:RegisterEffect(e1)
     -- ATK increase
@@ -21,7 +21,7 @@ function s.initial_effect(c)
     e2:SetCategory(CATEGORY_ATKCHANGE)
     e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
 	e2:SetRange(LOCATION_MZONE)
-    e2:SetTarget(s.atktg)
+    e2:SetTarget(s.target)
     e2:SetOperation(s.atkop)
     c:RegisterEffect(e2)
 end
@@ -32,6 +32,12 @@ end
 
 function s.sprfilter2(c)
     return c:IsType(TYPE_TRAP) and c:IsAbleToRemoveAsCost()
+end
+
+function s.sprcon(e, c)
+    if c == nil then return true end
+    return Duel.IsExistingMatchingCard(s.sprfilter1, c:GetControler(), LOCATION_MZONE, 0, 1, nil)
+        and Duel.IsExistingMatchingCard(s.sprfilter2, c:GetControler(), LOCATION_GRAVE, 0, 1, nil)
 end
 
 function s.sprop(e, tp, eg, ep, ev, re, r, rp, c)
